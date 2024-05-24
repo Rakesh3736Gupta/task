6d810b28a8c81b186b63f5e9571aec1904febdf0
Rakesh3736Gupta <rakeshgupta3736@gmail.com>
2024-05-24 09:18:28 +0530

    #

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const database = require("./config/database");
+const configurationRoutes = require('./routes/configurationRoutes');
+const dotenv = require("dotenv");
+const cors = require('cors');
+database.connect();
+
+dotenv.config();
+
+const app = express();
+
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api', configurationRoutes);
+
+
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
